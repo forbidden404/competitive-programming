3194db5a8bb48666dc34e185af4c7fd6cd240f48
Francisco Soares <francisco.ssn94@gmail.com>
2019-03-29 12:51:25 -0300

    UVa 11173 - Grey Codes

---
+++ UVA/11173.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+
+#define modulo(S, N) ((S) & (N-1))
+#define isPowerOfTwo(S) (!(S & (S-1)))
+#define nearestPowerOfTwo(S) ((int)pow(2.0, (int)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S-1))
+#define turnOnLastZero(S) ((S) | (S+1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S+1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S-1))
+
+int main() {
+    long n;
+    cin >> n;
+
+    while (n--) {
+        long a, b;
+        cin >> a >> b;
+        cout << (b ^ (b >> 1)) << endl;
+    }
+
+    return 0;
+}
